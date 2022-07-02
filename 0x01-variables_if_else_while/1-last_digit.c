b22bc0cfbc62a33137fe9d124a3929553bf2bed5
Oluwaseun25 <samuelawe842@gmail.com>
2022-07-02 04:51:43 +0100

    Hash tables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there*/
+	if (n > 5)
+		printf("%d and is greater than 5\n", n);
+	else if (n == 0)
+		printf("%d and is 0\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
